8a5575d947b373b6dd4c1014551688bb7c2b8e55
Mafurios <jeroen_vank_nr1@hotmail.com>
2025-11-22 22:19:56 +0100

    A007: CH3L5 Sprite updates amd drawings now work in groups

--- main.py
+++ main.py
@@ -10,23 +10,29 @@ def main():
 
     pygame.init()
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    running = True
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while running == True:
+    running = True
+    while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                running = False
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
